67c7055226af4d41405637ba82e03f49b6086097
yennacc <59937710+yennacc@users.noreply.github.com>
2020-06-04 21:30:17 -0700

    Add files via upload

---
+++ grc339test/js/parallax.js
@@ -0,0 +1,12 @@
+$(window).scroll(function () {
+var wScroll = $(this).scrollTop();
+
+$(".title").css({
+transform: "translate(0px, " + wScroll / 1.5 + "%)"
+});
+
+$(".transparent").css({
+transform: "translate(0px, -" + wScroll / 2 + "%)"
+});
+
+});
