75895f60afb7dee51b12c0adf846df7db97bdf55
yennacc <59937710+yennacc@users.noreply.github.com>
2020-06-11 23:45:18 -0700

    Add files via upload

---
+++ grc339final/js/script.js
@@ -0,0 +1,9 @@
+$(window).scroll(function () {
+  var wScroll = $(this).scrollTop();
+  $(".title").css({
+    transform: "translate(0px, " + wScroll / 1.5 + "%)"
+  });
+  $(".transparent").css({
+    transform: "translate(0px, -" + wScroll / 2 + "%)"
+  });
+});
